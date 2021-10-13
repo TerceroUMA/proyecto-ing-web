da371d4bb8465f17389e745f8e132fb541faae0d
David Castillo Berná <davidcastillo200.dc@gmail.com>
2021-10-13 23:06:52 +0200

    Creación Backend

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -10,9 +10,20 @@ export const About = () => {
         type: pruebaTypes.pruebaOn
     });
 
+    const handleSubmit = (e) => {
+        e.preventdefault();
+        fetch('localhost://8000/api/singIn/', {
+            method: 'POST'
+        })
+    }
+
     return (
         <div>
             <h1>About Screen</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text"/>
+                <button> Iniciar sesion</button>
+            </form>
         </div>
     );
 
