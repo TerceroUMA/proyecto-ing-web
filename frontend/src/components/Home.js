acfa15f35c8fe9a8ef9b1a92301dc4d61d6c2221
Javier <javi_roig@hotmail.es>
2021-10-14 01:27:22 +0200

    frontend preparado para deploy

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,15 +4,6 @@ import { pruebaTypes } from '../types/pruebaTypes';
 
 export const Home = () => {
 
-    fetch( 'https://localhost:8000', {
-        method: 'get'
-    })
-        .then( ( res ) => {
-
-            return res.json();
-
-        }).then( data => console.log( data ) );
-
     const dispatch = useDispatch();
 
     dispatch({
