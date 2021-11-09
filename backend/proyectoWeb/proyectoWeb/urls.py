4e925a1b6c30474cb5858d7c19822948c0fd4c12
David Castillo Berná <davidcastillo200.dc@gmail.com>
2021-11-09 20:16:49 +0100

    urls trayecto

--- backend/proyectoWeb/proyectoWeb/urls.py
+++ backend/proyectoWeb/proyectoWeb/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('login.urls')),
+    path('api/users/', include('login.urls')),
+    path('api/trayectos/', include('trayectos.urls')),
 ]
