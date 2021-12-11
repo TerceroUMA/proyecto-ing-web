636c500e4b739c935d1fc428aeebbea481369524
Javier <javi_roig@hotmail.es>
2021-12-11 12:53:30 +0100

    Drag and drop añadidod en registrarse

--- frontend/src/pages/Registrarse.jsx
+++ frontend/src/pages/Registrarse.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/pages/auth.css';
 
 import { useForm } from '../hooks/useForm';
 import { useHistory } from 'react-router';
 import { useDispatch } from 'react-redux';
 import { registrarse } from '../actions/auth';
+import { useDropzone } from 'react-dropzone';
 
 const Registrarse = () => {
 
   const dispatch = useDispatch();
-
   const history = useHistory();
 
   const [formValues, handleInputChange] = useForm({
@@ -24,6 +24,21 @@ const Registrarse = () => {
   });
 
   const { nombre, apellidos, correo, password, confirmarPassword, telefono, edad, localidad } = formValues;
+
+  const [file, setFile] = useState([]);
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: 'image/*',
+    onDrop: acceptedFiles => {
+
+      setFile(
+        acceptedFiles.map( file =>
+          Object.assign( file, {
+            preview: URL.createObjectURL( file )
+          })
+        ) );
+
+    }
+  });
 
   const sendToHome = () => {
 
@@ -55,90 +70,100 @@ const Registrarse = () => {
 
   return (
     <div className="auth__container">
-      <h1 className="auth__title">
+      <div className="registro">
+        <h1 className="auth__title">
         Registrarse
-      </h1>
-      <form
-        className="auth__form"
-        onSubmit={ handleOnSubmit }
-      >
-        {/* Se podría añadir */}
-        {/* <p style={{ alignSelf: 'flex-start', margin: 0 }}>Nombre de usuario: </p> */}
-        <input
-          className="sigIn__option form-control"
-          type="text"
-          name="nombre"
-          value={ nombre }
-          onChange={ handleInputChange}
-          placeholder="Nombre de usuario"
-        />
-        <input
-          className="sigIn__option form-control"
-          type="text"
-          name="apellidos"
-          value={ apellidos }
-          onChange={ handleInputChange}
-          placeholder="Apellidos"
-        />
-        <input
-          className="sigIn__option form-control"
-          type="email"
-          name="correo"
-          value={ correo }
-          onChange={ handleInputChange}
-          placeholder="Correo electrónico"
-        />
-        <input
-          className="sigIn__option form-control"
-          type="password"
-          name="password"
-          value={ password }
-          onChange={ handleInputChange}
-          placeholder="Contraseña"
-          autoComplete="off"
-        />
-        <input
-          className="sigIn__option form-control"
-          type="password"
-          name="confirmarPassword"
-          value={ confirmarPassword }
-          onChange={ handleInputChange}
-          placeholder="Confirmar contraseña"
-          autoComplete="off"
-        />
-        <input
-          className="sigIn__option form-control"
-          type="number"
-          name="telefono"
-          value={ telefono }
-          onChange={ handleInputChange}
-          placeholder="Telefono"
-        />
-        <input
-          className="sigIn__option form-control"
-          type="number"
-          name="edad"
-          value={ edad }
-          onChange={ handleInputChange}
-          placeholder="Edad"
-        />
+        </h1>
+        <form
+          className="auth__form"
+          onSubmit={ handleOnSubmit }
+        >
+          <input
+            className="sigIn__option form-control"
+            type="text"
+            name="nombre"
+            value={ nombre }
+            onChange={ handleInputChange}
+            placeholder="Nombre de usuario"
+          />
+          <input
+            className="sigIn__option form-control"
+            type="text"
+            name="apellidos"
+            value={ apellidos }
+            onChange={ handleInputChange}
+            placeholder="Apellidos"
+          />
+          <input
+            className="sigIn__option form-control"
+            type="email"
+            name="correo"
+            value={ correo }
+            onChange={ handleInputChange}
+            placeholder="Correo electrónico"
+          />
+          <input
+            className="sigIn__option form-control"
+            type="password"
+            name="password"
+            value={ password }
+            onChange={ handleInputChange}
+            placeholder="Contraseña"
+            autoComplete="off"
+          />
+          <input
+            className="sigIn__option form-control"
+            type="password"
+            name="confirmarPassword"
+            value={ confirmarPassword }
+            onChange={ handleInputChange}
+            placeholder="Confirmar contraseña"
+            autoComplete="off"
+          />
+          <input
+            className="sigIn__option form-control"
+            type="number"
+            name="telefono"
+            value={ telefono }
+            onChange={ handleInputChange}
+            placeholder="Telefono"
+          />
+          <input
+            className="sigIn__option form-control"
+            type="number"
+            name="edad"
+            value={ edad }
+            onChange={ handleInputChange}
+            placeholder="Edad"
+          />
 
-        <input
-          className="sigIn__option form-control"
-          type="text"
-          name="localidad"
-          value={ localidad }
-          onChange={ handleInputChange}
-          placeholder="Ciudad donde vive"
-        />
+          <input
+            className="sigIn__option form-control"
+            type="text"
+            name="localidad"
+            value={ localidad }
+            onChange={ handleInputChange}
+            placeholder="Ciudad donde vive"
+          />
 
 
-        <p style={{ wordWrap: 'break-word' }}>¿Ya tienes una cuenta? <a href="#"
-          onClick={ handleAnchor }>Inicia sesión</a> </p>
-        <button className="btn btn-primary">
+          <p style={{ wordWrap: 'break-word' }}>¿Ya tienes una cuenta? <a href="#"
+            onClick={ handleAnchor }>Inicia sesión</a> </p>
+          <button className="btn btn-primary">
             Registrarse
-        </button>
-      </form>
+          </button>
+        </form>
+      </div>
+      <div className="foto-container">
+        <h1 className="auth__title">Foto de perfil</h1>
+        <div className="drag-and-drop" {...getRootProps()}>
+          <input {...getInputProps()}/>
+          {
+            file.length > 0 ? <img style={{ maxWidth: '100%' }} src={file[0].preview} /> : <p>Arrastra una imagen o pinche aquí</p>
+          }
+        </div>
+      </div>
+
 
     </div>
   );
