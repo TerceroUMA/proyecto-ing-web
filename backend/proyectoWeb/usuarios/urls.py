c58deab5662785aa8194063a23009ac61deab4ae
David Castillo Berná <davidcastillo200.dc@gmail.com>
2022-01-14 17:03:59 +0100

    feature: oauth backend devuelve idUsuario

--- backend/proyectoWeb/usuarios/urls.py
+++ backend/proyectoWeb/usuarios/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Users, Login, Registrarse
+from .views import Users, Login, Registrarse, OAuth2
 urlpatterns = [
     path('', Users.as_view(), name='user'),
     path('/login', Login.as_view(), name="login"),
     path('/registrarse', Registrarse.as_view(), name="registrarse"),
+    path('/oauth2', OAuth2.as_view(), name="oauth2"),
 ]
